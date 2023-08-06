68871f7f7519aa5fb5239a0d8d0cb90abe0661b4
b4-tech <hcetanutrof@gmail.com>
2023-08-06 18:25:13 +0300

    feat: integrate Redux store with root component and set up ReactModal
    - Introduced React-Redux Provider to wrap App component.
    - Configured Redux-Persist with PersistGate for state persistence.
    - Initialized ReactModal with app root element.
    - Organized imports for better readability.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
+import 'normalize.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import ReactModal from 'react-modal';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App.tsx'
-import 'normalize.css';
+import store, { persistor } from './redux/store/store.ts';
+
+ReactModal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
